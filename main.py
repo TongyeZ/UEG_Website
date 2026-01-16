2e3760e1fdbb76478f19c209dac506e5b1346a72
TongyeZ <tzhang2005@outlook.com>
2026-01-15 22:40:11 -0600

    Initial commit: UEG Website project

--- main.py
+++ main.py
@@ -1,41 +1,11 @@
-# from flask import Flask, render_template, request, jsonify
-# import pandas as pd
-
-# app = Flask(__name__)
-
-# # Read the CSV file
-# ueg_data = pd.read_csv('/Users/sophiazhang/Desktop/UbiquitouslyExpressedGenes/data/Table 7. The global expression specificity and global expression patterns of human genes.csv')
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/graph')
-# def graph():
-#     return render_template('graph.html')
-
-# @app.route('/get_global_expression_specificity_data')
-# def get_global_expression_specificity_data():
-#     scatter_plot_data = {
-#         'Q50': ueg_data['Q50'].tolist(),
-#         'Q95':ueg_data['Q95'].tolist(),
-#         'Variance': ueg_data['Variance'].tolist(),
-#         'IQR': ueg_data['IQR'].tolist(),
-#         'Recount2_SEP_TPM_0_1': ueg_data['Recount2_SEP_TPM_0_1'].tolist(),
-#         'GeneID': ueg_data['GeneID'].tolist()
-#     }
-#     return jsonify(scatter_plot_data)
-
-# app.run(host='0.0.0.0', port=81)
-
-
+# main.py
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 
 app = Flask(__name__)
 
 # Read the CSV file
-ueg_data = pd.read_csv('/Users/sophiazhang/Desktop/UbiquitouslyExpressedGenes/data/Table 7. The global expression specificity and global expression patterns of human genes.csv')
+ueg_data = pd.read_csv('/Users/sophiazhang/Desktop/Projects/UEG_Website_Program/UbiquitouslyExpressedGenes/data/Table 7. The global expression specificity and global expression patterns of human genes.csv')
 
 @app.route('/')
 def index():
@@ -44,6 +14,10 @@ def index():
 @app.route('/graph')
 def graph():
     return render_template('graph.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
 
 @app.route('/get_global_expression_specificity_data')
 def get_global_expression_specificity_data():
